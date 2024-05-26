c40726982038d714d580875a61a4361e86de5de1
trishaolorvida <161606007+trishaolorvida@users.noreply.github.com>
2024-05-26 20:08:01 +0800

    Add files via upload

---
+++ 03_php_pdo_mysql_connection.php/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'config.php';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+
+    if ($pdo) {
+        echo "Connected to the $db database successfully!";
+
+        // execute a query
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
